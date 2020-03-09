37c6f8b7e709a2f6474802e952c96eaf882822c6
Jason <jasonk@outlook.co.nz>
2020-03-09 15:17:14 +1300

    Updated index.php

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatiable" content="IE=edge, chrome=1">
     <meta name="viewport" content="initial-scale=1, maximum-scale=1, width=device-width, height=device-height, shrink-to-fit=no">
 
-    <title>Weather</title>
+    <title>Weather App</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 </head>
