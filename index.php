5450be7a55f9944fbc99daa68cfa353603ba215f
Jason <jasonk@outlook.co.nz>
2020-03-09 15:08:49 +1300

    Added index.php

--- index.php
+++ index.php
@@ -9,7 +9,6 @@
     <title>Weather</title>
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <link rel="stylesheet" href="./public/assets/css/app.css">
 </head>
 <body>
 
